96b9d1fb20b7c935309738edab0656e22ba98858
Mityukova Lidia <LliMitOne@users.noreply.github.com>
2023-05-20 15:12:33 +0900

    Add window and label characteristics

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ tile = 10
 resolution = width, height = 1280, 720
 w, h = width // tile, height // tile
 pygame.init()
+
+window = pygame.display.set_mode(resolution)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Игра 'Жизнь'")
+
+font_main = pygame.font.SysFont('centurygothic', 100)
+font_rg = pygame.font.SysFont('centurygothic', 40)
