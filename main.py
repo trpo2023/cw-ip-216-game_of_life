058b2aae3d4f55de91277b06256f53181304e550
Mikhail Grorbunov <xiaomicrowave@users.noreply.github.com>
2023-05-20 16:46:52 +0700

    Add function to draw labels and create a main menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from cs_func import cell_status
+from dl_func import draw_label
 from random import randint
 from copy import deepcopy
 
@@ -38,6 +39,37 @@ def game_scene(cf, nf, color):
         pygame.display.flip()
         clock.tick(FPS)
 
-nf = [[0 for i in range(w)] for j in range(h)]
-cf = [[randint(0, 1) for i in range(w)] for j in range(h)]
-game_scene(cf, nf, 'orange')
+def main_menu():
+    c1 = randint(30, 44)
+    c2 = randint(14, 24)
+    nf = [[0 for i in range(w)] for j in range(h)]
+    cf = [[randint(0, 1) for i in range(w)] for j in range(h)]
+    cf1 = [[1 if not i % c1 else 0 for i in range(w)] for j in range(h)]
+    cf2 = [[1 if not (i * (j*4)) % c2 else 0 for i in range(w)] for j in range(h)]
+    while True:
+        window.fill(pygame.Color('black'))
+        draw_label("Игра 'Жизнь'", font_main, pygame.Color('white'), window, 330, 170)
+        draw_label('Random', font_rg, pygame.Color('orange'), window, 190, 430)
+        draw_label('Game', font_rg, pygame.Color('orange'), window, 205, 480)
+
+        draw_label('Waves', font_rg, pygame.Color('skyblue'), window, 560, 465)
+
+        draw_label('Football', font_rg, pygame.Color('green'), window, 890, 430)
+        draw_label('Yard', font_rg, pygame.Color('green'), window, 920, 480)
+
+        button_rg = pygame.draw.rect(window, pygame.Color('gray'), (150, 370, 250, 250), 10, 15)
+        button_p1 = pygame.draw.rect(window, pygame.Color('gray'), (500, 370, 250, 250), 10, 15)
+        button_p2 = pygame.draw.rect(window, pygame.Color('gray'), (850, 370, 250, 250), 10, 15)
+
+        mx, my = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+if __name__ == '__main__':
+    main_menu()
